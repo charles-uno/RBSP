42ec020773370899d613d949cd0b1d6985548aa3
Charles <mceachern@physics.umn.edu>
2016-03-23 10:59:40 -0500

    now let's plot some events...

--- finder.py
+++ finder.py
@@ -28,14 +28,14 @@ plotdir = '/home/user1/mceachern/Desktop/plots/' + now() + '/'
 
 def main():
 
-#  # Plot the location of the usable data. 
-#  return posplot(save='-i' in argv)
+  # Plot the location of the usable data. 
+  return posplot(save='-i' in argv)
 
 #  # Plot the location of events... ?
 #  return rateplot(save='-i' in argv)
 
-  # What dates do we have data for? 
-  dates = sorted( os.listdir('/media/My Passport/rbsp/pkls/') )
+#  # What dates do we have data for? 
+#  dates = sorted( os.listdir('/media/My Passport/rbsp/pkls/') )
 
 #  # If we're saving our data, nuke the previous list to avoid double-counting. 
 #  if '-i' in argv and os.path.exists('pos.txt'):
@@ -47,17 +47,17 @@ def main():
 #    print date
 #    [ trackpos(probe, date, mpc=5) for probe in ('a', 'b') ]
 
-  # If we're saving our data, nuke the previous list to avoid double-counting. 
-  if '-i' in argv and os.path.exists('events.txt'):
-    print 'Removing old event listing'
-    os.remove('events.txt')
+#  # If we're saving our data, nuke the previous list to avoid double-counting. 
+#  if '-i' in argv and os.path.exists('events.txt'):
+#    print 'Removing old event listing'
+#    os.remove('events.txt')
 
-  # Search for events. Do the days in random order, for easier debugging. We
-  # can just look at the first event we find. 
-  for date in np.random.permutation(dates):
-    print date
-    # Check both probes. Thirty minute chunks. 
-    [ checkdate(probe, date, mpc=30) for probe in ('a', 'b') ]
+#  # Search for events. Do the days in random order, for easier debugging. We
+#  # can just look at the first event we find. 
+#  for date in np.random.permutation(dates):
+#    print date
+#    # Check both probes. Thirty minute chunks. 
+#    [ checkdate(probe, date, mpc=30) for probe in ('a', 'b') ]
   return
 
 
@@ -210,33 +210,72 @@ def plotevent(ev, save=False):
 # ######################################################## Plot RBSP's Position
 # #############################################################################
 
-def posplot(save=False, unit='days'):
-  global plotdir
-#  unit = 'hours'
+# =============================================================================
+# ========================================================= Parse Position Data
+# =============================================================================
+
+def getpos(dl=0.5, dm=1, lmin=None, lmax=None, mmin=None, mmax=None, 
+           unit='days'):
+  # The options for units are hours and days. 
   secondsper = 86400. if unit=='days' else 1440.
   # The orbit of both RBSP paths has been broken into five-minute chunks. Grab
   # the position of each chunk which gives good data for E dot B = 0. 
   poslines = [ line for line in read('pos.txt') if line.endswith('ok') ]
-  # Tally up the total amount of usable data at five minutes per line, in days.
-  utime = 300*len(poslines)/secondsper
   # Get the date range. 
-  date0, date1 = poslines[0].split()[1], poslines[-1].split()[1]
-  # Parse the positions into an array of floats. 
+  dates = ( poslines[0].split()[1], poslines[-1].split()[1] )
+  # Arrange the positions as an array of floats. 
   pos = np.array( [ [ float(x) for x in p.split()[3:6] ] for p in poslines ] )
-  # Figure out appropriate bin sizes for the position data. 
-  dl, dm = 0.5, 1
-  lmin, lmax = np.floor( np.min( pos[:, 0] ) ), np.ceil( np.max( pos[:, 0] ) )
-  mmin, mmax = np.floor( np.min( pos[:, 1] ) ), np.ceil( np.max( pos[:, 1] ) )
-#  lmin, lmax = 4.5, 6.5
+  # Figure out the histogram bounds. 
+  if lmin is None:
+    lmin = np.floor( np.min( pos[:, 0] ) )
+  if lmax is None:
+    lmax = np.ceil( np.max( pos[:, 0] ) )
+  if mmin is None:
+    mmin = np.floor( np.min( pos[:, 1] ) )
+  if mmax is None:
+    mmax = np.ceil( np.max( pos[:, 1] ) )
+  # Number of bins in each direction. 
   lbins, mbins = int( (lmax - lmin)/dl ) + 1, int( (mmax - mmin)/dm ) + 1
-  # Bin the position data into a 2D histogram, then scale it to days. 
-  hist = np.histogram2d( pos[:, 0], pos[:, 1], bins=(lbins-1, mbins-1), 
-                         range=[ (lmin, lmax), (mmin, mmax) ] )[0]
-  z = 300*hist/secondsper
   # Bin bounds in terms of L and MLT. 
   l, m = np.mgrid[lmin:lmax:lbins*1j, mmin - dm/2.:mmax - dm/2.:mbins*1j]
   # Map to GSE coordinates. Put MLT=0 at the bottom. 
   x, y = l*np.sin(2*pi*m/24.), -l*np.cos(2*pi*m/24.)
+  # Bin the position data into a 2D histogram, then scale it to days. 
+  hist = np.histogram2d( pos[:, 0], pos[:, 1], bins=(lbins-1, mbins-1), 
+                         range=[ (lmin, lmax), (mmin, mmax) ] )[0]
+  z = 300*hist/secondsper
+  # Return the position data. Total amount of usable time too. 
+  return {'dates':dates, 'x':x, 'y':y, 'z':z}
+
+# =============================================================================
+# ============================================================ Parse Event Data
+# =============================================================================
+
+def getevents():
+
+  eventlines = [ line for line in read('events.txt') ]
+
+
+
+
+
+
+
+
+
+
+# =============================================================================
+# ===================================================== Plot Position Histogram
+# =============================================================================
+
+def posplot(save=False, unit='days'):
+  global plotdir
+  # Grab the position data. 
+  unit = 'days'
+  pos = getpos(unit=unit)
+  x, y, z = [ pos[key] for key in ('x', 'y', 'z') ]
+  date0, date1 = pos['dates']
+  dt = np.sum(z)
   # Create the plot window and scale the axes properly. 
   PW = plotWindow(square=True, colorbar='pos')
   lms, tks = (-8, 8), np.mgrid[-8:8:9j]
@@ -248,15 +287,10 @@ def posplot(save=False, unit='days'):
   xlbl, ylbl = 'Y' + notex(' (R_E)'), 'X' + notex(' (R_E)')
   ulabel = notex(unit)
   title = notex('Usable Data ' + date0 + ' to ' + date1 + ' (' + 
-                format(utime, '.0f') + ' ' + unit + ' total)')
+                format(dt, '.0f') + ' ' + unit + ' total)')
   PW.setParams(title=title, unitlabel=ulabel, xlabel=xlbl, ylabel=ylbl)
-  # Draw the grid. 
-  [ PW.setLine(x[i, :], y[i, :], 'k') for i in range( x.shape[0] ) ]
-  [ PW.setLine(x[:, j], y[:, j], 'k') for j in range( x.shape[1] ) ]
-  # Draw Earth. This is a bit kludgey. 
-  ax = ax = PW.cells.flatten()[0].ax
-  ax.add_artist( Wedge( (0, 0), 1, 0, 180, fc='w' ) )
-  ax.add_artist( Wedge( (0, 0), 1, 180, 360, fc='k' ) )
+  # Draw Earth. Mark the grid. 
+  PW.setParams(earth='top', grid=True)
   # Add the data to the plot. 
   PW.setMesh(x, y, z)
   # Show the plot, or save it as an image. 
@@ -264,6 +298,20 @@ def posplot(save=False, unit='days'):
     return PW.render(plotdir + 'pos.pdf')
   else:
     return PW.render()
+
+
+
+
+# =============================================================================
+# ===================================================== Plot Position Histogram
+# =============================================================================
+
+
+
+
+
+
+
 
 # #############################################################################
 # ############################################################# Plot Event Rate
